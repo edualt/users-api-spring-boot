a985847b2eb6a98b148633b416cf5718f9f05794
edualt <eduardosebastianhdz503@gmail.com>
2022-09-08 23:14:27 -0500

    feat(UserServiceImpl): new methods added

--- src/main/java/com/example/usersapispringboot/services/UserServiceImpl.java
+++ src/main/java/com/example/usersapispringboot/services/UserServiceImpl.java
@@ -1,4 +1,40 @@
 package com.example.usersapispringboot.services;
 
+import com.example.usersapispringboot.entities.User;
+import com.example.usersapispringboot.repositories.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserServiceImpl implements IUserService{
+
+    @Autowired
+    private IUserRepository repository;
+
+    @Override
+    public ResponseEntity<User> create(RequestEntity<User> request) {
+        User user = request.getBody();
+        assert user != null;
+        repository.save(user);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return null;
+    }
+
+    @Override
+    public User getUser(Long id) {
+        return null;
+    }
+
+    @Override
+    public User updateUser(Long id) {
+        return null;
+    }
 }
